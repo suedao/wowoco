a3cd76be667889c9f4bc23a323ca90651ddd6320
suedao <suedao@gmail.com>
2018-02-17 17:49:33 -0800

    .

--- ecard/Model/Cards.cs
+++ ecard/Model/Cards.cs
@@ -7,5 +7,8 @@ namespace ecard.Model
 {
     public class Cards
     {
+
+
+
     }
 }
--- ecard/Model/Greetings.cs
+++ ecard/Model/Greetings.cs
@@ -1,11 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ecard.Model
 {
     public class Greetings
     {
+        [Key]
+        public int? ID { get; set; }
+
+        [DisplayName("Your friend's name")]
+        public string friendsname { get; set; }
+
+        [DisplayName("Your friend's email")]
+        public string friendsemail { get; set; }
+
+        [DisplayName("Email Subject")]
+        public string subject { get; set; }
+
+        [DisplayName("Your Custom Message")]
+        public string message { get; set; }
+
+        [DisplayName("Your name")]
+        public string sendersname { get; set; }
+
+        [DisplayName("Your email")]
+        public string sendersemail { get; set; }
     }
 }
